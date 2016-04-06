34e832a51a9c02f3143e833af80d0187f965c2a6
efeciftci <efeciftci@gmail.com>
2016-04-06 23:31:36 +0300

    - added license information - added a random trigger that causes the mosquito to turn around without   hitting the sides - a little cleanup

--- src/com/efeciftci/mosquitosimulator/MosquitoSimulator.java
+++ src/com/efeciftci/mosquitosimulator/MosquitoSimulator.java
@@ -1,3 +1,18 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.efeciftci.mosquitosimulator;
 
 import java.util.Random;
@@ -13,7 +28,8 @@ public class MosquitoSimulator {
 
 	public static void main(String[] args) throws InterruptedException {
 		double x = 0.1, y = 1; // start position
-		double fx = 0.01, fy = 0.01; // how much will the mosquito move on each direction change
+		double fx = 0.01, fy = 0.01; // how much will the mosquito move on each
+										// direction change
 		String mf = "mr.jpg"; // path of mosquito image
 		XDir dx = XDir.right; // initial directions
 		YDir dy = YDir.bottom;
@@ -25,32 +41,32 @@ public class MosquitoSimulator {
 
 		while (true) {
 			StdDraw.clear();
-			if (x >= 1) {
+			if (x >= 1 || r.nextInt(100) < 4) {
 				dx = XDir.left;
 				mf = "ml.jpg";
 				fx = 0.01 * r.nextInt(5);
 			}
-			if (x <= 0) {
+			if (x <= 0 || r.nextInt(100) < 4) {
 				dx = XDir.right;
 				mf = "mr.jpg";
 				fx = 0.01 * r.nextInt(5);
 			}
-			if (y >= 1) {
+			if (y >= 1 || r.nextInt(100) < 4) {
 				dy = YDir.top;
 				fy = 0.01 * r.nextInt(5);
 			}
-			if (y <= 0) {
+			if (y <= 0 || r.nextInt(100) < 4) {
 				dy = YDir.bottom;
 				fy = 0.01 * r.nextInt(5);
 			}
 			if (dx == XDir.left)
-				x = x - (fx + 0.01 * r.nextInt(5) * (r.nextInt(2) == 1 ? 1 : -1));
+				x = x - (fx + 0.01 * r.nextInt(5) * (r.nextBoolean() ? 1 : -1));
 			if (dx == XDir.right)
-				x = x + (fx + 0.01 * r.nextInt(5) * (r.nextInt(2) == 1 ? 1 : -1));
+				x = x + (fx + 0.01 * r.nextInt(5) * (r.nextBoolean() ? 1 : -1));
 			if (dy == YDir.top)
-				y = y - (fy + 0.01 * r.nextInt(5) * (r.nextInt(2) == 1 ? 1 : -1));
+				y = y - (fy + 0.01 * r.nextInt(5) * (r.nextBoolean() ? 1 : -1));
 			if (dy == YDir.bottom)
-				y = y + (fy + 0.01 * r.nextInt(5) * (r.nextInt(2) == 1 ? 1 : -1));
+				y = y + (fy + 0.01 * r.nextInt(5) * (r.nextBoolean() ? 1 : -1));
 			StdDraw.picture(x, y, mf);
 			Thread.sleep(50);
 		}
